31bcc138b1236860516f4c3a0a3288d41fee3f7a
Apiyo4 <odingoannette@gmail.com>
2023-07-07 17:03:33 -0400

    fix: update file to physician-profile

--- ratesmd.py
+++ ratesmd.py
@@ -2,23 +2,32 @@ from pymongo import MongoClient
 from urllib.parse import quote_plus
 import csv
 import os
-from dotenv import load_dotenv
+import requests
+import pandas as pd
+import numpy as np
 
-load_dotenv()
+def read_chunks(file_path, size):
+    """
+    Read staging table in chunks because it has many rows. Returns a concatenated dataframe.
+    """
+    data_chunks = []
+    chunk_counter = 1
+    for chunk in pd.read_csv(file_path, chunksize=size, low_memory=False):
+        data_chunks.append(chunk)
+        chunk_counter += 1
+    dataset = pd.concat(data_chunks)
+    return dataset
 
-#fetch data from the cleaned-data2020.csv file
 def get_physicians_data_csv():
-    csv_file_path =  os.path.join('resources', 'cleaned-data2020.csv')
     doctors_details = list()
 
-    with open(csv_file_path, 'r') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)
-        for row in csv_reader:
-
-            doctors_details.append({'physician_first_name': row[2], 'physician_last_name': row[3],
-                                   'recipient_primary_business_street_address_line1': row[4], 'recipient_zip_code': row[7], 'physician_npi': row[1]})
-    return doctors_details
+    url = "https://download.cms.gov/openpayments/PHPRFL_P012023/OP_CVRD_RCPNT_PRFL_SPLMTL_P01202023.csv"
+    df = read_chunks(url, 10000)
+    select_columns = ['Covered_Recipient_Profile_First_Name', 'Covered_Recipient_Profile_Last_Name',
+                                   'Covered_Recipient_Profile_Address_Line_1','Covered_Recipient_Profile_Zipcode', 'Covered_Recipient_NPI']
+    doctors_details_df = df[select_columns]
+    doctors_details = doctors_details_df.values.tolist()
+    return doctors_details[1192089:]
 
 #fetch data from the ratesmd collection
 def get_doctors_data_rate(doctor_info):
@@ -26,22 +35,23 @@ def get_doctors_data_rate(doctor_info):
     return {
         'review_id': doctor_info["id"],
         'rating':  doctor_info["rating"]['average'],
+        'count': doctor_info["rating"]['count'],
     }
 
 # helper function for saving data to a csv file
 def save_data_to_csv(file_data):
-    file_path =  os.path.join('resources', 'doctors_ratings.csv')
     field_names = ['review_id',
                    'doctor_first_name',
                    'doctor_last_name',
                    'rating',
-                   'physician_npi'
+                   'physician_npi',
+                   'count'
 
                    ]
 
-    file_exists = os.path.isfile(file_path)
+    file_exists = os.path.isfile('doctors_ratings.csv')
 
-    with open(file_path, mode='a', newline='') as csv_file:
+    with open('doctors_ratings.csv', mode='a', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=field_names)
 
         if not file_exists:
@@ -57,14 +67,14 @@ def save_doctor_ratings_csv():
     for physician in physician_data:
 
         query = {
-            'location.postal_code': physician['recipient_zip_code'],  # 2832
+            'location.postal_code': physician[3],  # 2832
             "location.city.country_name": 'United States',
             "$or": [
                 {'full_name': {
-                    "$regex": f"^Dr\\. {physician['physician_first_name']} .* {physician['physician_last_name']}$"
+                    "$regex": f"^Dr\\. {physician[0]} .* {physician[1]}$"
 
                 }}, {'full_name': {
-                    "$regex": f"^Dr\\. {physician['physician_first_name']} {physician['physician_last_name']}$"
+                    "$regex": f"^Dr\\. {physician[0]} {physician[1]}$"
 
                 }}
             ]
@@ -74,10 +84,10 @@ def save_doctor_ratings_csv():
             "location.city.country_name": 'United States',
             "$or": [
                 {'full_name': {
-                    "$regex": f"^Dr\\. {physician['physician_first_name']} .* {physician['physician_last_name']}$"
+                    "$regex": f"^Dr\\. {physician[0]} .* {physician[1]}$"
 
                 }}, {'full_name': {
-                    "$regex": f"^Dr\\. {physician['physician_first_name']} {physician['physician_last_name']}$"
+                    "$regex": f"^Dr\\. {physician[0]} {physician[1]}$"
 
                 }}
             ]
@@ -88,22 +98,20 @@ def save_doctor_ratings_csv():
         if doctor_info:
             doctor_data_rate = get_doctors_data_rate(doctor_info)
             dr_info = {
-                'doctor_first_name':  physician['physician_first_name'],
-                'doctor_last_name':  physician['physician_last_name'],
-                'physician_npi': physician['physician_npi']
+                'doctor_first_name':  physician[0],
+                'doctor_last_name':  physician[1],
+                'physician_npi': physician[4]
 
             }
             save_data_to_csv({**doctor_data_rate, ** dr_info })
             
 
 # connect to database 
-
-user = os.getenv('user')
-password = os.getenv('pwd')
-host = os.getenv('host')
-
+user = "c1-ds4a-2-team-23"
+password = "PhobRoswuBropRaKUM9R"
+host = "20.232.135.212:27017"
 uri = "mongodb://%s:%s@%s" % (
-    quote_plus(user.encode('utf-8')), quote_plus(password.encode('utf-8')), quote_plus(host.encode('utf-8')))
+    quote_plus(user), quote_plus(password), host)
 client = MongoClient(uri)
 
 db = client['healthrate']
@@ -112,3 +120,4 @@ ratemdDB = db.ratemd
 
 # Invoke main function to save data to doctors_rating.csv
 save_doctor_ratings_csv()
+
